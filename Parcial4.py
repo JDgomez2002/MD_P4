11e269baecf2acef5857733f7899b9a5dc0030a0
JDgomez <87883400+JDgomez2002@users.noreply.github.com>
2022-11-27 22:29:51 -0600

    Program header

---
+++ Parcial4.py
@@ -0,0 +1,8 @@
+#Universidad del Valle de Guatemala
+#Matematica Discreta I
+#Seccion 10
+#Catedratico Mario Castillo
+#Segundo Semestre 2022
+#Jose Daniel Gomez Cabrera 21429
+#Actividad: Parcial Practico 4
+
